0ab6cec612ce354c1abc4cbe40e579b310284cf0
kirubell2 <kirubeletat093@gmailcom>
2022-12-08 11:47:50 -0800

    me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-/**
- *Main -Entry point
- *
- *Return: Always o (sucess)
- */
+
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle\n";
-	puts(str);
-	return 0;
-}
-
+       	puts ("Programming is like building a multilingual puzzle\n");
+       	return (0); 
+ }
