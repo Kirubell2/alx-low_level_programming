05fbb160b3d855a76ad6b222d996efe5ec840b8b
kirubell2 <kirubeletat093@gmailcom>
2022-12-09 14:39:53 -0800

    me

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>                                                                                                                     
+#include <time.h>                                                                                                                       
+#include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
+ * main - main block                                                                                                                    
+ * Description: get a random number and check the last digit, compare it with 5
+ * Return: 0                                                                                                                             */                                                                                                                                     
+int main(void)                                                                                                                          
+{
+       	int n;
+	int last;
+
+	srand(time(0));    
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+	if (last > 5)
+		printf("last digit of %i is greater than 5\n", n);
+	else if (last == 0)
+		printf("last digit of %i and is o\n", n);                                                                            
+     	else                                
+		printf("last digit of %i and is less than 6 and not 0\n", n);
+	return (0);                                                                                                                     
+}                                                                                                                                       
