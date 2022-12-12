a1dcb51e6e3aa1ab115ea2e97f3d07a181d51381
kirubell2 <kirubeletat093@gmailcom>
2022-12-12 12:46:58 -0800

    me

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{												
+{
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
