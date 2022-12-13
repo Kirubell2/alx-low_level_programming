861b07ad12d8eb668bd67e887bb5cf7dc5cc9071
kirubell2 <kirubeletat093@gmailcom>
2022-12-13 15:42:50 -0800

    me

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,23 +1,17 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- *
- * main - check the code.
- *  
- * Return: Always 0.
- *
- */
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int _putchar(char c);
 
-int main(void
-{
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
+#endif
