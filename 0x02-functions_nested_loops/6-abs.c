4e2bddd2da92f6a5efc3d53fba47dcd731e53b41
kirubell2 <kirubeletat093@gmailcom>
2022-12-14 08:39:02 -0800

    me

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,11 @@
 #include "main.h"
-
 /**
- * _abs -> computes the absolute value of an integer
- * @int: parameter is integers
- * Return: always 0
+ *_abs - Check Holberton
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
  */
+
 int _abs(int r)
 {
 	if (r >= 0)
