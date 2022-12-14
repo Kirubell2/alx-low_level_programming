e8edc323eada84f542e868c5125d759d6b9a02a4
kirubell2 <kirubeletat093@gmailcom>
2022-12-14 13:18:56 -0800

    me

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int add(int, int);
+	int sum;
+
+	sum = add(89, 0);
+
+	printf("%d", sum);
+	return (0);
+}
