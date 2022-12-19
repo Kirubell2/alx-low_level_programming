4337b7bc48741b51ea94b9b3d5a938a75230fce8
kirubell2 <kirubeletat093@gmailcom>
2022-12-19 13:46:21 -0800

    me

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-IN_H                                                                                                                          
-
 #ifndef MAIN_H                                                                 
 #define MAIN_H
 #include <stdio.h>
