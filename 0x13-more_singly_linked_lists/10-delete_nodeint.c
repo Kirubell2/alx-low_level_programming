21ba830d0d247f2e5e91fd8810c06f52e7df8f6f
kirubell2 <kirubeletat093@gmailcom>
2023-01-31 12:36:01 -0800

    more singlr links

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		while (i < index)
 		{
-			temp = temp->next
+			temp = temp->next;
 			if (temp == NULL)
 				return (-1);
 			i++;
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,11 +15,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t))
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	if (*head == NULL
+	if (*head == NULL)
 	{
 		*head = new_node;
 		new_node->next = NULL;
@@ -34,7 +34,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-
 	temp = *head;
 	while (i < idx)
 	{
